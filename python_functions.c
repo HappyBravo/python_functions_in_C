509d4abfb1729e9d89895a2f022a4a2325a05f23
Happy <83241650+HappyBravo@users.noreply.github.com>
2022-02-10 23:24:34 +0530

    added few more function
    Added list function:
    > len()
    > append()
    > pop()
    > insert()
    > remove()

--- python_functions.c
+++ python_functions.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-// typedef struct
-// {
-//     int length;
-//     int elements[];
-// }list;
+struct element
+{
+    int data;
+    struct element *next;
+    struct element *prev;
+};
 
+typedef struct element node;
 
+// %%%%%%%%%%%%%% TAKING INPUT %%%%%%%%%%%%%%
 char *python_input(char s[])
 {
     FILE *fp = stdin;
@@ -35,25 +38,148 @@ char *python_input(char s[])
     return str;
 }
 
-// int python_len(int *array[])
-// {
-//     printf("\nstart %d", array);
-//     printf("\nend %d", (*array+1));
+// %%%%%%%%%%%%%% FINDING LENGTH OF ARRAY %%%%%%%%%%%%%%
+int python_len(node *start)
+{
+    int count = 0;
+    node* temp;
+    temp = start;
+    if (start == NULL)
+        return 0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        count += 1;
+    }
+    return count;
+}
+
+// %%%%%%%%%%%%%% ADDING ELEMENT AT THE END OF ARRAY %%%%%%%%%%%%%%
+node* python_append(node *start, int data)
+{
+	node *p,*tmp;
+	tmp = (node *)malloc(sizeof(node));
+	tmp->data = data;
+	p=start;
+	while(p->next != NULL)
+		p = p->next;
+	p->next = tmp;
+	tmp->next=NULL;
+	// display(start);
+	return start;
+}
+
+// %%%%%%%%%%%%%% REMOVING ELEMENT FROM END OF ARRAY %%%%%%%%%%%%%%
+node* python_pop(node* start)
+{
+    node *p;
+    p=start;
+	while(p->next->next != NULL)
+		p = p->next;
+    free(p->next);
+    p->next = NULL;
+
+    return start;
+}
+
+
+// %%%%%%%%%%%%%% INSERTING ELEMENT AT GIVEN INDEX IN ARRAY %%%%%%%%%%%%%%
+node* python_insert(node *head, int value, int index)
+{
+    node* temp, *newNode;
+    index = index-1;
+    int index_c = 0;
+    if (head == NULL)
+    {
+        // ERROR MESSAGE
+        printf("List is empty");
+        exit(0);
+    }
+    newNode = (node*)malloc(sizeof(node));
+    newNode->data = value;
     
-//     // int count = 0;
-//     // int temp[] = {};
-//     // printf("\nSize of array : %d", *(&array+1)-array);
-//     // for (int i = 0; i<sizeof(*array); i++)
-//     // {
-//     //     printf("\n- %d ", array[i]);
-//     // //     temp[i] = array[i];
-//     // //     printf("\n%d", temp[i]);
-//     // }
+    if (index == -1)
+    {
+        // ADDING ELEMENT AT THE BEGINNING
+        // printf("Adding at beg");
+        newNode->next = head;
+        head = newNode;          
+    }
+    
+    temp = head;
+    
+    while((temp!=NULL))
+    {
+        if (index == index_c)
+        {
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+        else
+            temp=temp->next;
+        index_c +=1;
+    }
+    if (index == index_c-1) 
+    {
+        // ADDING VALUE AT THE END
+        python_append(head, value);
+    }
+    if (index >= index_c) 
+    {
+        // ERROR MESSAGE
+        printf("Value not entered as index was greater than Length of array");
+    }
+    return head;
+}
 
-//     return &array[1]-array;
-// }
+// %%%%%%%%%%%%%% REMOVING ELEMENT FROM GIVEN INDEX IN ARRAY %%%%%%%%%%%%%%
+node* python_remove(node* head, int index)
+{
+    node* temp, *newNode, *newhead;
+    index = index-1;
+    int index_c = 0;
+    
+    if (head == NULL)
+    {
+        // ERROR MESSAGE
+        printf("List is empty");
+        exit(0);
+    }
 
-// int python_append(int *list[], int *tobeadded[])
-// {
-//     return 0;
-// }
+    newhead = head;
+    if (index == -1)
+    {
+        // printf("Removing from beg");
+        // REMOVING ELEMNT FROM THE BEGINNING
+        newhead = head->next;
+        free(head);
+        index_c += 1;          
+    }
+    
+    temp = newhead;
+    
+    
+    while((temp->next->next != NULL))
+    {
+        if (index == index_c)
+        {
+            newNode = temp->next->next;
+            free(temp->next);
+            temp->next = newNode;
+        }
+        else
+            temp=temp->next;
+        index_c += 1;
+    }
+    if (index == index_c) 
+    {
+        // REMOVING ELEMENT FROM END
+        python_pop(head);
+    }
+    if (index >= index_c) 
+    {
+        // ERROR MESSAGE
+        printf("Value not Removed as index was greater than Length of array");
+    }
+    return newhead;
+}
